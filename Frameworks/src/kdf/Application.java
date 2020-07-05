13a0afafb6f7ad7d7ae69d8d8b798e99c6a13629
Balaji <Balaji@LAPTOP-PP7LTPN0>
2020-07-05 20:50:20 +0530

    Saving Frameworks project to GIT

---
+++ Frameworks/src/kdf/Application.java
@@ -0,0 +1,48 @@
+package kdf;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import utils.GenericMethods;
+
+public class Application {
+
+	@Test
+	public void verifyInvalidLogin() throws IOException {
+		String[][] data = GenericMethods.getData("D:\\SelMar29\\TestData.xlsx", "Sheet2");
+		for(int i = 1;i<data.length;i++) {
+			switch(data[i][3]) {
+			case "openBrowser":
+				Methods.openBrowser();
+				break;
+			case "maxBrowser":
+				Methods.maximizeBrowserWindow();
+				break;
+			case "impWait":
+				Methods.implicitWait();
+				break;
+			case "navigateToApp":
+				Methods.navigateToApplication(data[i][6]);
+				break;
+			case "enterUserID":
+				Methods.enterUserId(data[i][4],data[i][5], data[i][6]);
+				break;
+			case "enterPassword":
+				Methods.enterPassword(data[i][4],data[i][5], data[i][6]);
+				break;
+			case "clickButton":
+				Methods.clickButton(data[i][4],data[i][5]);
+				break;
+			case "verifyErrMsg":
+				String actualMsg = Methods.getErrMsg(data[i][5]);
+				Assert.assertEquals(actualMsg, data[i][6]);
+				break;
+			case "closeApplication":
+				Methods.closeApplicationWindow();
+				break;
+			}
+		}
+	}
+}
